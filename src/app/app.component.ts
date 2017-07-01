d034b70de89a68d191fd6fbd8eb02215c21f519c
MadhuAdiga <madhu.vah@gmail.com>
2017-07-01 09:45:53 +0530

    SeedProject_Assignment1

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';//Import Component
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>Hello {{name}}</h1><br>
+            <h2> SeedProject : Assignment1 </h2>
+            <b><p><b>Problem Statement: </b> Create setup for any Angular 2 Application with following given Technology stack.
+
+              ● Angular 2
+
+              ● Gulp
+
+              ● Jasmine
+
+              ● Karma
+
+              ● SASS
+
+              ● Typescript
+
+              ● Webpack
+
+              ● Webpack Development Server</p>`,
+})
+
+
+export class AppComponent  { name = 'Angular'; }
